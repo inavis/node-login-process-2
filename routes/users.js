2de0da5cd9dabdadd832c0e6cfdf5fa1a7e92e2a
Sivani Saravanan <rednaz.inavis@gmail.com>
2022-02-03 11:55:09 +0530

    date change

--- routes/users.js
+++ routes/users.js
@@ -173,9 +173,9 @@ router.post("/forgot-password",async (request,response)=>{
 
         // new Date object
         const currentdate = new Date(); 
-        const datetime = (currentdate.getDate() + "-"
-        + (currentdate.getMonth()+1)  + "-" 
-        + currentdate.getFullYear() + "-"  
+        const datetime = (currentdate.getDate() + "/"
+        + (currentdate.getMonth()+1)  + "/" 
+        + currentdate.getFullYear() + " "  
         + currentdate.getHours() + ":"  
         + currentdate.getMinutes() + ":" 
         + currentdate.getSeconds()); 
