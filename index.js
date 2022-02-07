4bb6feb9e860cfc291eb43816c300b9a90c2906e
Sivani Saravanan <rednaz.inavis@gmail.com>
2022-02-07 22:06:58 +0530

    changes

--- index.js
+++ index.js
@@ -22,8 +22,13 @@ async function createConnection(){
 export const client = await createConnection();
 
 //middleware
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "https://url-shortener-1.netlify.app/"); // Update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 app.use(express.json())
-app.use(cors());
+// app.use(cors());
 
 
 //routes
