16b516a14dfab2d08eb69563fc7bd6b4ce5135d2
Sivani Saravanan <rednaz.inavis@gmail.com>
2022-02-08 11:30:47 +0530

    changes

--- index.js
+++ index.js
@@ -22,19 +22,7 @@ async function createConnection(){
 export const client = await createConnection();
 
 //middleware
-const corsOptions = {
-    origin: '*'
-  }
-  app.use(cors(corsOptions))
-  
-  app.use(function(req, res, next) {
-      res.header("Access-Control-Allow-Origin", '*');
-      res.header("Access-Control-Allow-Credentials", true);
-      res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-      res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-      next();
-  });
-// app.use(cors())
+app.use(cors())
 app.use(express.json())
 
 //routes
